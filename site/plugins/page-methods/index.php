deb335e62f5b889ebdc23259012eb698e0485ecf
jaumeferretevazquez <jaume.ferrete@gmail.com>
2021-07-01 13:37:09 +0200

    Helper page methods for works

---
+++ site/plugins/page-methods/index.php
@@ -0,0 +1,25 @@
+<?php
+Kirby::plugin('jaume/page-methods', [
+	'pageMethods' => [
+		'includedInShows' => function () {
+			$shows = new Pages();
+            return page("shows")->children()->filter(function ($show) use ($shows) {
+				foreach($show->worksIncluded()->toPages() as $work) {
+					if ($work === $this) {
+						$shows->add($show);
+					}
+				}
+				return $shows;
+			});	
+		},
+		'dateOrOngoing' => function () {
+			if ($this->ongoing()->toBool() === true) {
+				return ' -> Ongoing';
+			} else if ($this->dateend()->isNotEmpty()) {
+				return ' -> ' . $this->dateend();
+			} else {
+				return '';
+			}
+		}
+	]
+]);

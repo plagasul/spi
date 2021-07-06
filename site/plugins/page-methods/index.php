ee73977783b39611f8f6ab1eb7f9667cd75f1c01
jaumeferretevazquez <jaume.ferrete@gmail.com>
2021-07-06 20:15:31 +0200

    Fixed includedInShows custom page method

--- site/plugins/page-methods/index.php
+++ site/plugins/page-methods/index.php
@@ -3,14 +3,16 @@ Kirby::plugin('jaume/page-methods', [
 	'pageMethods' => [
 		'includedInShows' => function () {
 			$shows = new Pages();
-            return page("shows")->children()->filter(function ($show) use ($shows) {
+            page("shows")->children()->filter(function ($show) use ($shows) {
 				foreach($show->worksIncluded()->toPages() as $work) {
 					if ($work === $this) {
 						$shows->add($show);
 					}
 				}
-				return $shows;
+
 			});	
+			// return $this->title();
+			return $shows;			
 		},
 		'dateOrOngoing' => function () {
 			if ($this->ongoing()->toBool() === true) {

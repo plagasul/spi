c84397dc86e6effe7a6ac3ba07240e2a98df47a1
jaumeferretevazquez <jaume.ferrete@gmail.com>
2021-07-06 19:55:20 +0200

    Hook that gives incremental unique codes to work

--- site/config/config.php
+++ site/config/config.php
@@ -3,4 +3,19 @@
 return [
     'languages' => true,
     'debug'  => true,
+    'hooks' => [
+        'page.create:after' => function ($page, $input) {
+            if ($page->intendedTemplate() == "work") {
+                $index = site()->workIndex()->toInt();
+                $index++;
+                $index = str_pad(strval($index), 3, "0", STR_PAD_LEFT);
+                $page->update([
+                    'unique' => $index . '-CS'
+                ]);
+                site()->update([
+                    'workIndex' => $index
+                ]);
+            }
+        },
+    ]
 ];

e281eaa4459003e3fd97669a2ef8a8f9db72e74c
jaumeferretevazquez <jaume.ferrete@gmail.com>
2021-11-08 01:23:04 +0100

    garden logo

--- site/snippets/navbar.php
+++ site/snippets/navbar.php
@@ -3,5 +3,8 @@
         <div class="works navbar-item"><a href="<?= page("works")->url() ?>"><?= page("works")->title()->lower()->html() ?></a></div>
         <div class="shows navbar-item"><a href="<?= page("shows")->url() ?>"><?= page("shows")->title()->lower()->html() ?></a></div>
         <div class="info navbar-item"><a href="<?= page("info")->url() ?>"><?= page("info")->title()->lower()->html() ?></a></div>
-        <div class="garden navbar-item"><a href="<?= page("garden")->url() ?>">&</a></div>
+        <div class="garden navbar-item"><a href="<?= page("garden")->url() ?>">
+            <?php $garden = asset('assets/svg/garden.svg') ?>
+            <img src="<?= $garden->url() ?>" />
+        </a></div>
 </nav>

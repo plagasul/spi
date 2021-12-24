49c2c2775708991290dbc01183dc7fe5fecd7aaa
jaumeferretevazquez <jaume.ferrete@gmail.com>
2021-12-24 17:03:42 +0100

    Better flexbox navbar

--- site/snippets/navbar.php
+++ site/snippets/navbar.php
@@ -1,10 +1,14 @@
+<?php $garden = asset('assets/svg/garden.svg') ?>
 <nav class="navbar">
-        <div class="logo navbar-item"><a href=""><?= $site->title()->html() ?></div>
-        <div class="works navbar-item"><a href="<?= page("works")->url() ?>"><?= page("works")->title()->lower()->html() ?></a></div>
-        <div class="shows navbar-item"><a href="<?= page("shows")->url() ?>"><?= page("shows")->title()->lower()->html() ?></a></div>
+    <div class="navbar-top">
+        <div class="garden navbar-item">
+            <a href="<?= page("garden")->url() ?>"><img src="<?= $garden->url() ?>" /></a>
+        </div>
         <div class="info navbar-item"><a href="<?= page("info")->url() ?>"><?= page("info")->title()->lower()->html() ?></a></div>
-        <div class="garden navbar-item"><a href="<?= page("garden")->url() ?>">
-            <?php $garden = asset('assets/svg/garden.svg') ?>
-            <img src="<?= $garden->url() ?>" />
-        </a></div>
+        <div class="shows navbar-item"><a href="<?= page("shows")->url() ?>"><?= page("shows")->title()->lower()->html() ?></a></div>
+        <div class="works navbar-item"><a href="<?= page("works")->url() ?>"><?= page("works")->title()->lower()->html() ?></a></div>
+    </div>
+    <div class="navbar-bottom">
+        <div class="logo navbar-item"><a href=""><?= $site->title()->html() ?></a></div>
+    </div>
 </nav>

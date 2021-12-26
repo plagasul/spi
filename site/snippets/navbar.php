eaf83622214318cf621931dc69a7696c626fda2b
jaumeferretevazquez <jaume.ferrete@gmail.com>
2021-12-26 19:28:18 +0100

    navbar items active status

--- site/snippets/navbar.php
+++ site/snippets/navbar.php
@@ -4,11 +4,11 @@
         <div class="garden navbar-item">
             <a href="<?= page("garden")->url() ?>"><img src="<?= $garden->url() ?>" /></a>
         </div>
-        <div class="info navbar-item"><a href="<?= page("info")->url() ?>"><?= page("info")->title()->lower()->html() ?></a></div>
-        <div class="shows navbar-item"><a href="<?= page("shows")->url() ?>"><?= page("shows")->title()->lower()->html() ?></a></div>
-        <div class="works navbar-item"><a href="<?= page("works")->url() ?>"><?= page("works")->title()->lower()->html() ?></a></div>
+        <div class="info navbar-item <?= $page == page("info") ? 'active' : '' ?>"><a href="<?= page("info")->url() ?>"><?= page("info")->title()->lower()->html() ?></a></div>
+        <div class="shows navbar-item <?= $page == page("shows") ? 'active' : '' ?>"><a href="<?= page("shows")->url() ?>"><?= page("shows")->title()->lower()->html() ?></a></div>
+        <div class="works navbar-item <?= $page == page("works") ? 'active' : '' ?>"><a href="<?= page("works")->url() ?>"><?= page("works")->title()->lower()->html() ?></a></div>
     </div>
     <div class="navbar-bottom">
-        <div class="logo navbar-item"><a href=""><?= $site->title()->html() ?></a></div>
+        <div class="logo navbar-item"><a href="<?= $site->url() ?>"><?= $site->title()->html() ?></a></div>
     </div>
 </nav>

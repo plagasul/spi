aa56e8fdc9e2cf2a6f8eb9573b1e944890ce508d
jaumeferretevazquez <jaume.ferrete@gmail.com>
2021-12-26 23:34:36 +0100

    works, sidebar

--- site/snippets/sidebar-works.php
+++ site/snippets/sidebar-works.php
@@ -1,7 +1,7 @@
 <nav class="sidebar-works" style="background-color:<?= $site->sidebarcolor() ?>">
     <?php foreach(page('works')->children() as $work): ?>
         <?php if ($work->intendedTemplate() != 'gap'): ?>
-            <div class="sidebar-works-work"><?= $work->title()->html() ?></div>
+            <div class="sidebar-works-work" data-work-target="<?= $work->slug() ?>"><?= $work->title()->html() ?></div>
         <?php else: ?>
             <div class="sidebar-works-gap">&nbsp;</div>
         <?php endif?>
--- site/templates/works.php
+++ site/templates/works.php
@@ -1,4 +1,5 @@
 <?php snippet('html-pre') ?>
 <?php snippet('navbar') ?>
 <?php snippet('sidebar-works') ?>
+<?php snippet('content-works') ?>
 <?php snippet('html-post') ?>

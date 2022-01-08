c9ca85db26090cc5d735b57716b2979c2c218c22
jaumeferretevazquez <jaume.ferrete@gmail.com>
2022-01-08 13:18:02 +0100

    Works element dimensions logic

--- site/snippets/content-works.php
+++ site/snippets/content-works.php
@@ -11,9 +11,28 @@
                 <?php endforeach ?>
             </div>
             <h1 class="content-works-title"><?= $work->title() ?></h1>
-            <div class="content-work-date"><?= $work->dateStart()->isNotEmpty() ? $work->datestart()->toDate('Y') : 'n.d.'?><?= $work->ongoing()->toBool() ? ' - ongoing' : '' ?><?= $work->dateend()->isNotEmpty() ? ' - ' . $work->dateend()->toDate('Y') : '' ?></div>
+            <div class="content-work-date">
+                <?= $work->datestart()->isNotEmpty() ? $work->datestart() : 'n.d.'?>
+                <?= $work->ongoing()->toBool() ? ' - ongoing' : '' ?>
+                <?= $work->ongoing()->toBool() == false && $work->dateend()->isNotEmpty() ? ' - ' . $work->dateend() : '' ?>
+            </div>
             <?php foreach ($work->elementdimensions()->toStructure() as $element): ?>
-                <div class="content-works-details"></div>
+                <div class="content-works-elementDescription"><?= $element->elementdescription()->kt() ?></div>
+                <div class="content-works-elementSize">
+                    <?php if ($element->variableDimensions()->toBool()): ?>
+                        dimensions variable
+                    <?php else: ?>
+                        <?= $element->width()->isNotEmpty() ? $element->width() : ''?>
+                        <?= $element->height()->isNotEmpty() ? ' x ' . $element->height() : ''?>
+                        <?= $element->length()->isNotEmpty() ? ' x ' . $element->length() : ''?>
+                        <?= 
+                            $element->width()->isNotEmpty() ||                        $element->height()->isNotEmpty() || 
+                            $element->length()->isNotEmpty() ? ' cm ' : '' 
+                        ?>             
+                        <?= $element->diameter()->isNotEmpty() ? 'ø ' . $element->diameter() . ' cm' : ''?>
+                        <?= $element->duration()->isNotEmpty() ? $element->duration()->toDate('G\hi\'s\'\'') : '' ?>
+                    <?php endif ?>
+                </div>
             <?php endforeach ?>
             <div class="content-works-description"><?= $work->description()->kt() ?></div>
             <?php $showsIncluded = $work->includedInShows() ?>
